ccdcc0ad024552bdab11c649699fcd30fb171881
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-27 13:49:30 +0530

    [LakshmiGanesh] refactor : refactor to introduce csv builder exception to ensure that csv builder encapsulate all changes

---
+++ src/main/java/com/capgemini/census/CustomFileIOException.java
@@ -0,0 +1,12 @@
+package com.capgemini.census;
+
+enum ExceptionTypeIO{
+	FILE_PROBLEM
+}
+
+@SuppressWarnings("serial")
+public class CustomFileIOException extends Exception {
+	public CustomFileIOException(ExceptionTypeIO exceptionType) {
+		super(exceptionType.toString());
+	}
+}
