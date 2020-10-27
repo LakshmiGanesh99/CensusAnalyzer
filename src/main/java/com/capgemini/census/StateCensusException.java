7b960991fb9c7a75e294be30b94f3696347aca32
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-27 09:36:17 +0530

    [LakshmiGanesh] add:Ability for the analyzer to load the Indian States Census Information from a csv file

---
+++ src/main/java/com/capgemini/census/StateCensusException.java
@@ -0,0 +1,14 @@
+package com.capgemini.census;
+
+public class StateCensusException extends Exception {
+	StateCensusExceptionType exceptionType;
+
+	public StateCensusException(String message, StateCensusExceptionType exceptionType) {
+		super(message);
+		this.exceptionType = exceptionType;
+	}
+}
+
+enum StateCensusExceptionType {
+	CENSUS_FILE_PROBLEM, INCORRECT_TYPE, SOME_OTHER_ERRORS
+}
