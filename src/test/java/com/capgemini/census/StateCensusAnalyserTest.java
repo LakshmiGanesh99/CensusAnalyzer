ffa3c72ed3288ada7308b8b9e5eea171db39de99
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-27 09:52:30 +0530

    [LakshmiGanehs] TC1.2: check if custom exception is returned if incorrect data is provided.

--- src/test/java/com/capgemini/census/StateCensusAnalyserTest.java
+++ src/test/java/com/capgemini/census/StateCensusAnalyserTest.java
@@ -1,16 +1,28 @@
 package com.capgemini.census;
 
 import org.junit.Assert;
-import org.junit.Test; 
+import org.junit.Test;
 
 public class StateCensusAnalyserTest {
 	private final String STATE_CENSUS_CSV_FILE = "./src/main/resources/StateCensusCSVData.csv";
 	private final String INCORRECT_STATE_CENSUS_CSV_FILE = "./src/main/resources/_StateCensusCSVData.csv";
- 
+
 	@Test
 	public void givenTheStatesCensusCSVFile_WhenRead_NoOfRecordsShouldMatch() throws StateCensusException {
 		StateCensusAnalyzer stateCensusAnalyser = new StateCensusAnalyzer();
 		int numOfRecords = stateCensusAnalyser.loadTheStateCensusCSVData(STATE_CENSUS_CSV_FILE);
 		Assert.assertEquals(6, numOfRecords);
+	}
+
+	@Test
+	public void givenIncorrectCSVFile_ShouldReturnCustomException() {
+		String exceptionMessage = null;
+		try {
+			StateCensusAnalyzer stateCensusAnalyser = new StateCensusAnalyzer();
+			stateCensusAnalyser.loadTheStateCensusCSVData(INCORRECT_STATE_CENSUS_CSV_FILE);
+		} catch (StateCensusException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(StateCensusExceptionType.CENSUS_FILE_PROBLEM.toString(), exceptionMessage);
 	}
 }
